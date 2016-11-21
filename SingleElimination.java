d2f5af73fe3a39d27fc7ada744fe1e4fc1d69121
nakidaazevedo <na8138@mun.ca>
2016-11-21 10:43:34 -0330

    Add files via upload

--- SingleElimination.java
+++ SingleElimination.java
@@ -1,22 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class SingleElimination implements Format{
 
-
 	@SuppressWarnings("null")
 	@Override
-	public Team[] setSchedule(Tournament tournament){			//set the schedule for the tournament
+	public void setSchedule(Tournament tournament){			//set the schedule for the tournament
 		List<Team> teamList = tournament.getTeams();
-		Team[] bracket = null;
+		Team[] randTeamList = null;
 		Random rand = new Random();
 		
-		for (int i=0; i < teamList.size() -1; i++) {
-			int r = rand.nextInt(teamList.size() + 1) + 1;
+		for (int i=0; i < teamList.size(); i++) {
+			int r = rand.nextInt(teamList.size()) + 1;
 			
 			if (r <= teamList.size()) {
 				Team teamName = teamList.get(r);
-				bracket[i] = teamName;
+				randTeamList[i] = teamName;
 				teamList.remove(r);
 			}
 			else {
@@ -24,18 +24,33 @@ public class SingleElimination implements Format{
 			}
 			
 		}
-		return bracket;
-	}
-
-	@Override
-	public Team[] getSchedule(Tournament tournament){
-		return setSchedule(tournament);
+		
+			List<Match> matches = new ArrayList();
+			int i = 0;
+			while (i <= teamList.size()) {
+				if ((teamList.size() % 2) != 1) {
+					Match match = new Match();
+					match.setTeam1(randTeamList[i]);
+					match.setTeam2(randTeamList[i+1]);
+					matches.add(match);
+					i = i+2;
+				} else {
+					teamList.remove(rand.nextInt(teamList.size()) + 1);
+					break;
+				}
+			}
 	}
 
 	@Override
 	public void editSchedule(Tournament tournament) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public Team[] getSchedule(Tournament tournament) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
