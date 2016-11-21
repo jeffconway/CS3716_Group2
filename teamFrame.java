42e61820a9b2948c0b04ce07936591ac06ced238
Shawnie <sds660@mun.ca>
2016-11-21 10:51:26 -0330

    Add files via upload

---
+++ teamFrame.java
@@ -0,0 +1,25 @@
+import java.awt.*; 
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+class teamFrame extends JFrame {
+	JLabel name,deadline;
+	JPanel p1,p2;
+	
+	teamFrame(Tournament T) {
+		setTitle("Edit Team");
+		setSize(400,400);
+   		setLayout(new GridLayout(2, 1));
+		p1 = new JPanel();
+      	p1.setLayout(new FlowLayout());
+		p2 = new JPanel();
+      	p2.setLayout(new FlowLayout());
+		name = new JLabel("Name: " + t.getName());
+		deadline = new JLabel("Deadline: " + t.getDeadline());
+		p1.add(name);
+		p2.add(deadline);
+		this.add(p1);
+		this.add(p2);
+	}
+}
