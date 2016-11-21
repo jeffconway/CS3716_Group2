76130d46773cf3de191ae4a24f5f63f4bfff8d54
Shawnie <sds660@mun.ca>
2016-11-21 10:05:35 -0330

    Updated TournamentInit and shortened/fixed lines.

--- TournamentInit.java
+++ TournamentInit.java
@@ -10,70 +10,53 @@ public class TournamentInit {
    	private JPanel controlPanel,controlPanel2,controlPanel3;
 	private java.util.List<Tournament> tournaments = new ArrayList();
 	private JTextField name, deadline;
+	private JButton createTournament,checkTournaments;
 
-   public TournamentInit(){
-      prepareGUI();
-   }
-
-	private void prepareGUI(){
-   		f = new JFrame("Tournament Organizer");
+  	public TournamentInit(){
+      	f = new JFrame("Tournament Organizer");
       	f.setSize(400,400);
-      	f.setLayout(new GridLayout(4, 1));
-      	f.addWindowListener(new WindowAdapter() {
-        public void windowClosing(WindowEvent windowEvent){
-          	System.exit(0);
-       	}        
-  	});    
-
+   		f.setLayout(new GridLayout(4, 1));
+   		f.addWindowListener(new WindowAdapter() {
+        	public void windowClosing(WindowEvent windowEvent){
+          		System.exit(0);
+       		}        
+  		});    
 		controlPanel = new JPanel();
       	controlPanel.setLayout(new FlowLayout());
 		controlPanel2 = new JPanel();
     	controlPanel2.setLayout(new FlowLayout());
 		controlPanel3 = new JPanel();
     	controlPanel3.setLayout(new FlowLayout());
-
+		createTournament = new JButton("Create Tournament");
+		checkTournaments = new JButton("Check Tournaments");
+		createTournament.setHorizontalTextPosition(SwingConstants.LEFT);   	
+		createTournament.addActionListener(new ActionListener() {
+		  	public void actionPerformed(ActionEvent e) {
+		   		tournamentGUI();
+		 	}
+		});
+		checkTournaments.addActionListener(new ActionListener() {
+     		public void actionPerformed(ActionEvent e) {
+        		listGUI();
+      		}
+   		});
+      	controlPanel2.add(createTournament); 
+	  	controlPanel3.add(checkTournaments);  
 		f.add(controlPanel);
 		f.add(controlPanel2);
-		f.add(controlPanel3);
-      	f.setVisible(true);  
-   }  
-
+		f.add(controlPanel3);     
+      	f.setVisible(true); 
+ 	} 
 	private void tournamentGUI() {
 		f1 = new TournamentFrame(tournaments);
 		f1.setVisible(true);
 	}
-
-	private void checkGUI() { 
+	private void listGUI() { 
 		f2 = new ListFrame(tournaments);
 		f2.setVisible(true);
 	}
 
-	   private void TournamentInit(){
-;
-		  JButton createTournament = new JButton("Create Tournament");
-		  JButton checkTournaments = new JButton("Check Tournaments");
-		  createTournament.setHorizontalTextPosition(SwingConstants.LEFT);   
-
-		  createTournament.addActionListener(new ActionListener() {
-		     public void actionPerformed(ActionEvent e) {
-		        tournamentGUI();
-		     }
-		  });
-	
-		checkTournaments.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-            checkGUI();
-         }
-      });
-
-      controlPanel2.add(createTournament); 
-	  controlPanel3.add(checkTournaments);       
-
-      f.setVisible(true);  
-   }
-
 	public static void main(String[] args){
-      TournamentInit  init = new TournamentInit();      
-      init.TournamentInit();
+      TournamentInit  init = new TournamentInit();
    }
 }
