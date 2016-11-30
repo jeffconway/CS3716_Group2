26cf306ff7ac69b747f0f9883c6d419549238bc4
jeffconway <d46jlc@mun.ca>
2016-11-30 09:34:24 -0330

    added comments

--- ListFrame.java
+++ ListFrame.java
@@ -2,10 +2,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class ListFrame extends JFrame {
-	/**
-	 * 
-	 */
+class ListFrame extends JFrame {												/*class for creating and displaying the list of tournaments*/
+
 	private static final long serialVersionUID = 1L;
 	TeamFrame f;
  	JPanel p1,listPane,p2;
@@ -15,7 +13,7 @@ class ListFrame extends JFrame {
 	static java.util.List<Tournament> to;
 	private JFrame fra;
 
- 	ListFrame(JFrame fr,java.util.List<Tournament> tournaments) {
+ 	ListFrame(JFrame fr,java.util.List<Tournament> tournaments) {				/*creates the frame to display the tournaments*/
 		fra = fr;
 		to = tournaments;
 		fra.setTitle("List Teams");
@@ -24,7 +22,7 @@ class ListFrame extends JFrame {
       	listPane.setLayout(new BoxLayout(listPane,BoxLayout.Y_AXIS));
 		p2 = new JPanel();
 		p2.setLayout(new FlowLayout());
-		ba = new JButton("Back");
+		ba = new JButton("Back");												/*back button*/
 		ba.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				removePanels();
@@ -32,7 +30,7 @@ class ListFrame extends JFrame {
 		   	}          
 		});
 		p2.add(ba);
-		for (i=0; i<tournaments.size();i++) {
+		for (i=0; i<tournaments.size();i++) {									/*gets all the tournments from the list of tournaments*/
 			p1 = new JPanel();
             p1.setPreferredSize(new Dimension(400,65));
             p1.setMaximumSize(p1.getPreferredSize());
@@ -40,7 +38,7 @@ class ListFrame extends JFrame {
 			p1.add(l1);
 			l2 = new JLabel("Deadline: \t"+tournaments.get(i).getDeadline());
 			p1.add(l2);
-			JButton b = new JButton("Edit Tournament");
+			JButton b = new JButton("Edit Tournament");							/*edit tournament button (one for each tournament)*/
 			p1.add(b);
 			b.addActionListener(new ActionListener() {
 				final int j=i+1;
@@ -58,7 +56,7 @@ class ListFrame extends JFrame {
 		fra.revalidate();
 		fra.repaint();
 	}
-	private void removePanels() {
+	private void removePanels() {												/*removes the panels from the frame so the updated panels can be added*/
 		fra.remove(listPane);
 		fra.remove(p2);
 	}
