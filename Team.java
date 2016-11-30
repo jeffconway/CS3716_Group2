28817a2295281801a7066216f36cc00e4074bf22
jeffconway <d46jlc@mun.ca>
2016-11-30 12:29:50 -0330

    updated comments

--- Team.java
+++ Team.java
@@ -2,59 +2,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
-public class Team implements Serializable{
+public class Team implements Serializable{				/* class to create a team object */
 
-	String name;								//name of team
-	List<Player> players = new ArrayList<Player>();		//list of player names
+	String name;										/* name of team */
+	List<Player> players = new ArrayList<Player>();		/* list of player names */
 	List<Player> coaches = new ArrayList<Player>();
-	int wins = 0, losses = 0;					//total wins and losses for the team, each object starts with 0
+	int wins = 0, losses = 0;							/* total wins and losses for the team, each object starts with 0 */
 
-	public Team(String name){					//constructor for team
+	public Team(String name){							/* constructor for team */
 		this.name = name;
 	}
 
-	public String getTeamName(){				//get team name
+	public String getTeamName(){						/* get team name */
 		return name;
 	}
 
-	public List<Player> getPlayers(){			//get all players on team
+	public List<Player> getPlayers(){					/* get all players on team */
 		return players;
 	}
 
 	
-	public List<Player> getCoaches(){			//get all coaches
+	public List<Player> getCoaches(){					/* get all coaches */
 		return(coaches);		
 	}	
 
-	public int getWins(){						//get the number of wins for the team
+	public int getWins(){								/* get the number of wins for the team */
 		return wins;
 	}
 
-	public int getLosses(){						//get the number of losses for the team
+	public int getLosses(){								/* get the number of losses for the team */
 		return losses;
 	}
 
-	public void setTeamName(String name){		//set team name
+	public void setTeamName(String name){				/* set team name */
 		this.name = name;
 	}	
 
-	public void addPlayer(Player newPlayer){	//add a player to the team
+	public void addPlayer(Player newPlayer){			/* add a player to the team */
 		players.add(newPlayer);
 	}
 
-	public void addCoach(Player newCoach){		//add a coach to the team
+	public void addCoach(Player newCoach){				/* add a coach to the team */
 		coaches.add(newCoach);
 	}
 	
-	public void addWin(){						//add a win
+	public void addWin(){								/* add a win */
 		wins++;
 	}
 
-	public void addLoss(){						//add a loss
+	public void addLoss(){								/* add a loss */
 		losses--;
 	}
 
-	public void delPlayer(Player delPlayer){			//remove a player from the team		
+	public void delPlayer(Player delPlayer){			/* remove a player from the team */	
 		for(int i = 0; i <= players.size(); i++){
 			Player temp = players.get(i);
 			if((temp.name.compareTo(delPlayer.name)) == 0){
@@ -62,10 +62,9 @@ public class Team implements Serializable{
 				return;
 			}
 		}
-	}
-	
-	
-	public void delCoach(Player delCoach){				//remove a coach from the team		
+	}	
+
+	public void delCoach(Player delCoach){				/* remove a coach from the team	*/
 		for(int i = 0; i <= coaches.size(); i++){
 			Player temp = coaches.get(i);
 			if((temp.name.compareTo(delCoach.name)) == 0){
@@ -75,11 +74,11 @@ public class Team implements Serializable{
 		}
 	}
 
-	public void removeWin(){					//remove a win
+	public void removeWin(){							/* remove a win */
 		wins--;
 	}
 
-	public void removeLoss(){					//remove a loss
+	public void removeLoss(){							/* remove a loss */
 		losses--;
 	}
 }
