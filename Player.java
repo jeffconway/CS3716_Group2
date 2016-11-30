ae87a888a0a2199e264ed9c4d8a8940606bc52fe
jeffconway <d46jlc@mun.ca>
2016-11-30 12:26:44 -0330

    updated comments

--- Player.java
+++ Player.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
-public class Player implements Serializable{
+public class Player implements Serializable{			/* class to create a player object with the players details */
 
-	String name, gender;								//player name and gender
-	int age;											//player age
-	float height, weight;								//player height and weight
+	String name, gender;								/* player name and gender */
+	int age;											/* player age */
+	float height, weight;								/* player height and weight */
 
-	public Player(String name, String gender, int age, float height, float weight){		//constructor for player with all variables
+	public Player(String name, String gender, int age, float height, float weight){		/* constructor for player object with all variables */
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
@@ -14,47 +14,47 @@ public class Player implements Serializable{
 		this.weight = weight;
 	}
 
-	public Player(String name){							//constructor for player with only name given
+	public Player(String name){							/* constructor for player with only name given */
 		this.name = name;
 	}
 
-	public String getName(){							//get player name
+	public String getName(){							/* get player name */
 		return name;
 	}
 
-	public int getAge(){								//get player age
+	public int getAge(){								/* get player age */
 		return age;
 	}
 
-	public float getHeight(){							//get player height
+	public float getHeight(){							/* get player height */
 		return height;
 	}
 
-	public float getWeight(){							//get player weight
+	public float getWeight(){							/* get player weight */
 		return weight;
 	}
 
-	public String getGender(){							//get player gender
+	public String getGender(){							/* get player gender */
 		return gender;		
 	}
 
-	public void setName(String name){				//set player name
+	public void setName(String name){					/* set player name */
 		this.name = name;
 	}
 
-	public void setAge(int age){					//set player age
+	public void setAge(int age){						/* set player age */
 		this.age = age;
 	}
 
-	public void setHeight(float height){			//set player height
+	public void setHeight(float height){				/* set player height */
 		this.height = height;
 	}
 
-	public void setWeight(float weight){			//set player weight
+	public void setWeight(float weight){				/* set player weight */
 		this.weight = weight;
 	}
 
-	public void setGender(String gender){			//set player gender
+	public void setGender(String gender){				/* set player gender */
 		this.gender = gender;
 	}
 }
