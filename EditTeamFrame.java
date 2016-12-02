07651b5ec5ea170d8412265f58ae5785e76fdc26
jeffconway <d46jlc@mun.ca>
2016-12-02 11:39:08 -0330

    Add files via upload

--- EditTeamFrame.java
+++ EditTeamFrame.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 
-class EditTeamFrame extends JFrame {
+class EditTeamFrame extends JFrame {																	/*class to create the edit team frame*/
 	static Tournament to;
 	static java.util.List<Tournament> tournaments;
 	static Team te;
@@ -14,7 +14,7 @@ class EditTeamFrame extends JFrame {
 	JPanel p1,p2,p3,p4,pp1;
 	JButton b1,b2,b3,b4,bb;
 
-	EditTeamFrame(int index,Tournament t,JFrame fr,java.util.List<Tournament> tour) {
+	public EditTeamFrame(int index,Tournament t,JFrame fr,java.util.List<Tournament> tour) {			/*constuctor*/
 		to = t;
 		f = fr;
 		tournaments = tour;
@@ -23,7 +23,7 @@ class EditTeamFrame extends JFrame {
 		f.setTitle("Edit Team");
 		f.setSize(600,600);
    		f.setLayout(new GridLayout(4, 1));
-		
+
 		p1 = new JPanel();
       	p1.setLayout(new FlowLayout());
 		p2 = new JPanel();
@@ -32,16 +32,16 @@ class EditTeamFrame extends JFrame {
       	p3.setLayout(new BoxLayout(p3,BoxLayout.Y_AXIS));
 		p4 = new JPanel();
       	p4.setLayout(new FlowLayout());
-		
+
 		tf1 = new JTextField(te.getTeamName(),20);
-		b1 = new JButton("Back");
+		b1 = new JButton("Back");																		/*back button*/
 		b1.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				removePanels();
 				f1 = new TeamFrame(to,f,tournaments);
-		   	}          
+		   	}
 		});
-		b2 = new JButton("Submit");
+		b2 = new JButton("Submit");																		/*submit button*/
 		b2.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				removePanels();
@@ -50,7 +50,7 @@ class EditTeamFrame extends JFrame {
 				f1 = new TeamFrame(to,f,tournaments);
 		   	}          
 		});
-		b3 = new JButton("Delete");
+		b3 = new JButton("Delete");																		/*delete button*/
 		b3.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				tournaments.get(tournaments.indexOf(to)).getTeams().remove(te);
@@ -59,26 +59,26 @@ class EditTeamFrame extends JFrame {
 				f1 = new TeamFrame(to,f,tournaments);
 		   	}          
 		});
-		b4 = new JButton("Add Player");
+		b4 = new JButton("Add Player");																	/*add player button*/
 		b4.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				removePanels();
 				f1 = new AddPlayerFrame(ind,to,f,tournaments);
-		   	}          
+		   	}
 		});
 		for (i=0; i<te.getPlayers().size();i++) {
 			pp1 = new JPanel();
 			l1 = new JLabel((i+1)+". "+te.getPlayers().get(i).getName());
 			pp1.add(l1);
-			JButton bb1 = new JButton("Edit Player");
+			JButton bb1 = new JButton("Edit Player");													/*edit player button*/
 			bb1.addActionListener(new ActionListener() {
 				final int j=i;
 		   		public void actionPerformed(ActionEvent e) {
 					removePanels();
 					ff = new AddPlayerFrame(j,ind,to,f,tournaments);
-		   	 	}          
+		   	 	}
 		  	});
-			JButton bb2 = new JButton("Delete Player");
+			JButton bb2 = new JButton("Delete Player");													/*delete player button*/
 			bb2.addActionListener(new ActionListener() {
 				final int j=i;
 		   		public void actionPerformed(ActionEvent e) {
@@ -87,19 +87,19 @@ class EditTeamFrame extends JFrame {
 					removePanels();
 					updateFile();
 					ff = new EditTeamFrame(ind,to,f,tournaments);
-		   	 	}          
+		   	 	}
 		  	});
 			pp1.add(bb1);
 			pp1.add(bb2);
 			p3.add(pp1);
 		}
-	
+
 		p1.add(b1);
 		p1.add(b3);
 		p2.add(tf1);
 		p4.add(b2);
 		p4.add(b4);
-	
+
 		f.add(p1);
 		f.add(p2);
 		f.add(p3);
@@ -107,14 +107,14 @@ class EditTeamFrame extends JFrame {
 		f.revalidate();
 		f.repaint();
 	}
-	
-	private void removePanels() {
+
+	private void removePanels() {														/*method to remove panels so the new panels can be added*/
 		f.remove(p1);
 		f.remove(p2);
 		f.remove(p3);
 		f.remove(p4);
 	}
-	private void updateFile() {
+	private void updateFile() {															/*method to update the file that stores the tournaments */
 		try {
 				FileOutputStream fileOut = new FileOutputStream("data.ser");
 				ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -123,10 +123,10 @@ class EditTeamFrame extends JFrame {
 				fileOut.close();
 			}
 			catch(FileNotFoundException ex) {
-				ex.printStackTrace();		
+				ex.printStackTrace();
 			}
 			catch(IOException ex) {
-				ex.printStackTrace();		
-			}
+				ex.printStackTrace();
+		}
 	}
 }
