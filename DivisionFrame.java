6f533720d77033ef989bbfb9ad8fe27e93626ae1
jeffconway <d46jlc@mun.ca>
2016-12-02 11:23:13 -0330

    Add files via upload

--- DivisionFrame.java
+++ DivisionFrame.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class DivisionFrame extends JFrame{
+public class DivisionFrame extends JFrame{											/*class to create a display the Divisions frame, for view schedule*/
 	JLabel j1,j2;
 	JButton b1;
 	JPanel p1;
@@ -11,44 +11,28 @@ public class DivisionFrame extends JFrame{
     Team t1;
     Match m1;
 	SingleElimination s1;
-	Divisions d;
-	
+	Divisions d;	
 	Tournament tt;
-	TournamentFrame tou1;   // JRadioButton single, division;
+	TournamentFrame tou1;	
 	
-	
-	public DivisionFrame( Divisions d1){
-		d=d1;
-		
-		setTitle("Schedule Frame");
-		
+	public DivisionFrame(Divisions d1){												/*constructor for the frame*/
+		d=d1;		
+		setTitle("Schedule Frame");		
 		setSize(600,400);
 		
-		p1=new JPanel();
-		
-		j1= new JLabel("Divisions Schedule: ");
-		
-		f1 = new JTextArea();    // Division schedule to be displayed in here
-		
-		//f1.setText.toString(s1.getSchedule(tt));
+		p1=new JPanel();		
+		j1= new JLabel("Divisions Schedule: ");		
+		f1 = new JTextArea();
 
-		String[] names = new String[tt.getTeams().size()];    // 
+		String[] names = new String[tt.getTeams().size()];							/*list of team names*/
 		d1.setSchedule(tt);
 		
-		for(int i=0; i<tt.getTeams().size(); i++){
-			names[i] =  tt.getTeams().get(i).getTeamName();   // get the name of teams and store them in the name list
-			f1.setText(names[i]);       // display each of team in the text area  to be displayed  
+		for(int i=0; i<tt.getTeams().size(); i++){				
+			names[i] =  tt.getTeams().get(i).getTeamName();							/* get the name of teams and store them in the name list */
+			f1.setText(names[i]);       											/* display each of team in the text area  to be displayed */
 			
-		}
-		
-		p1.add(j1);
-		
+		}		
+		p1.add(j1);		
 		p1.add(f1);
 	}
-	
-	//public static void main(String[] args){
-	//}
-	
-	
-
 }
