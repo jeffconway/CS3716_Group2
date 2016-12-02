ae8f65cd33227580b841fd20579d408bf72586c1
jeffconway <d46jlc@mun.ca>
2016-12-02 11:19:07 -0330

    Add files via upload

--- AddTeamFrame.java
+++ AddTeamFrame.java
@@ -3,10 +3,8 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 
-public class AddTeamFrame extends JFrame {
-	/**
-	 * 
-	 */
+public class AddTeamFrame extends JFrame {											/*class to create a frame to create/edit a team*/	
+
 	private static final long serialVersionUID = 1L;
 	JLabel l1, errorLabel;
 	JTextField t1;
@@ -19,12 +17,12 @@ public class AddTeamFrame extends JFrame {
 	static Team te;
 	private JFrame f;
 
-	public AddTeamFrame(Tournament t,JFrame fr,java.util.List<Tournament> tour) {
+	public AddTeamFrame(Tournament t,JFrame fr,java.util.List<Tournament> tour) {	/*constructor*/
 		f = fr;
 		tournaments = tour;
 		to = t;
 		te = new Team("");
-		f.setTitle("Add Team");
+		f.setTitle("Add Team");														/*add team button*/
 		f.setSize(600,400);
 		f.setLayout(new GridLayout(4, 1));
 		p1 = new JPanel();
@@ -35,16 +33,16 @@ public class AddTeamFrame extends JFrame {
 		p4.setLayout(new FlowLayout());
 		errorLabel = new JLabel("");
 
-		l1 = new JLabel("Team Name: ");
+		l1 = new JLabel("Team Name: ");												/*field to enter team name*/
 		t1 = new JTextField(20);
 		p1.add(l1);
 		p1.add(t1);
-		b2 = new JButton("Submit Team");
+		b2 = new JButton("Submit Team");											/*submit team button*/
 		b2.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
-		   		if (t1.getText().trim().isEmpty()) {
+		   		if (t1.getText().trim().isEmpty()) {								/*checks if name field is filled*/
 		   			errorLabel.setForeground(Color.RED);
-					errorLabel.setText("Must fill out all fields to Add Team!");
+					errorLabel.setText("Must fill out all fields to Add Team!");	/*prints a warning if the name field is empty*/
 		   		} else {
 				String name = t1.getText().trim();
 				te.setTeamName(name);
@@ -55,7 +53,7 @@ public class AddTeamFrame extends JFrame {
 		   		}
 		   	 }          
 		 });
-		b3 = new JButton("Back");
+		b3 = new JButton("Back");													/*back button*/
 		b3.addActionListener(new ActionListener() {
 		   	public void actionPerformed(ActionEvent e) {
 				removePanels();
@@ -72,12 +70,12 @@ public class AddTeamFrame extends JFrame {
 		f.revalidate();
 		f.repaint();
 	}
-	private void removePanels() {
+	private void removePanels() {													/*method to remove panels so the new panels can be added*/
 		f.remove(p1);
 		f.remove(p3);
 		f.remove(p4);
 	}
-	private void updateFile() {
+	private void updateFile() {														/*method to update the file that stores the tournaments */
 		try {
 				FileOutputStream fileOut = new FileOutputStream("data.ser");
 				ObjectOutputStream out = new ObjectOutputStream(fileOut);
