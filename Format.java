dca62a4c0a5d404ed3d8535dea08bbc33f8d634c
jeffconway <d46jlc@mun.ca>
2016-12-02 11:42:10 -0330

    Add files via upload

--- Format.java
+++ Format.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
-public interface Format{
+public interface Format{									/*interface for the various formats*/
 	public String getType();
 
-	public void setSchedule(Tournament tournament);			//set the schedule for the tournament
+	public void setSchedule(Tournament tournament);			/*set the schedule for the tournament*/
 
-	public void editSchedule(Tournament tournament);	//edit the tournament schedule
+	public void editSchedule(Tournament tournament);		/*edit the tournament schedule*/
 
-	Team[] getSchedule(Tournament tournament);
+	Team[] getSchedule(Tournament tournament);				/*get the tournament schedule*/
 }
